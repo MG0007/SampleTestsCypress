5b5e6e3e1fb0a4a2cf26d6d1e531bf0aac333f8e
MG0007 <mgdreamspersonalgithub@gmail.com>
2025-01-03 19:28:56 +0200

    Test to confirm all card in Component page display Visit button

--- cypress/e2e/framerTestCases.js
+++ cypress/e2e/framerTestCases.js
@@ -19,14 +19,36 @@ describe('Framer home page loads and links work as expected', () => {
     it('Pricing tiles display "$" for product prices', () => {
         cy.get(locators.pricingPage.pageUrl).click();
 
-        cy.get(locators.pricingPage.pageTiles).each((tile) =>{
+        cy.get(locators.pricingPage.pageTiles).each((tile) => {
             expect(tile.text()[0]).to.eq('$');
         });
     });
 
-    it('FAQ section in Pricing page contains 4 questions', ()=>{
+    it('FAQ section in Pricing page contains 4 questions', () => {
         cy.get(locators.pricingPage.pageUrl).click();
 
         cy.get(locators.pricingPage.pageQuestions).should('have.length', locators.pricingPage.fqaNumberOfQuestions);
+    });
+
+    it.only('Does display "visit" button on each card in Components section of Components page', () => {
+        cy.get(locators.componentsPage.pageUrl).click();
+
+        let visitButtonTotal = 0;
+        let largeCardTotal = 0;
+        let smallCardTotal = 0;
+
+        cy.get(locators.componentsPage.cardsVisitButtons).its('length').then((buttonCount) => {
+            visitButtonTotal = buttonCount;
+        });
+
+        cy.get(locators.componentsPage.largeCard).its('length').then((cardCount) => {
+            largeCardTotal = cardCount;
+        });
+
+        cy.get(locators.componentsPage.smallCard).its('length').then((cardCount) => {
+            smallCardTotal = cardCount;
+
+            expect(largeCardTotal + smallCardTotal).to.equal(visitButtonTotal);
+        });
     });
 });
